54cc75f29e0dbc3836e4bbb789052516a46f5ed1
somyasrishti111 <somyasrishti111@gmail.com>
2024-11-21 10:43:56 +0530

    Add files via upload

---
+++ mysql table.sql
@@ -0,0 +1,12 @@
+create database delshiv;
+Create table students(Rollno int unsigned,
+firstname varchar(50),
+mobileno varchar(10),
+emailid varchar(100),
+address varchar(200),
+gender varchar(50),
+age int,
+DOB date);
+insert into student values(1,'somya','7856047043','somyasrishti@124gmail.com','sakarnagar','female','26','1998-12-28');
+select* from student;
+select* from students;
